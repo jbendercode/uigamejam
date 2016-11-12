dd55dec11798edb889fbf73fcd55b820f6b534bb
Jacky Zhang <zhg.jacky@gmail.com>
2016-11-12 01:59:08 -0500

    Added DrawLine

---
+++ GameJam/Assets/OurAssets/Scripts/DrawLine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawLine : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (((Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved)
+		    || Input.GetMouseButton (0))) {
+
+			Plane objPlane = new Plane (Camera.main.transform.forward *= 1, this.transform.position);
+
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float rayDistance;
+			if (objPlane.Raycast (ray, out rayDistance)) {
+				this.transform.position = ray.GetPoint (rayDistance);
+			}
+		}
+	}
+}
